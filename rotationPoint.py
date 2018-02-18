b0cf3ffc5a15c8ae26585170e37f148034f49f15
Johann Wentzel <wentzel142@gmail.com>
2018-02-17 23:30:18 -0700

    Completed rotationPoint.py

--- rotationPoint.py
+++ rotationPoint.py
@@ -1,4 +1,20 @@
+def rotationPoint(words):
+	low = 0
+	high = len(words) - 1
+	index = (low + high) // 2
 
+	while low <= high:
+		index = (low + high) // 2
+
+		if words[index] < words[index - 1]:
+			return index
+
+		if words[index] < words[low]:
+			high = index
+		else: 
+			low = index
+
+	return index
 
 if __name__ == '__main__':
 	words = [
@@ -15,4 +31,4 @@ if __name__ == '__main__':
     'othellolagkage',
 	]
 
-	print(words)
+	print(rotationPoint(words))
